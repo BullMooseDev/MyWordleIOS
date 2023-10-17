9bf22237d1a139064daed74de42e725845fac672
Kole Ervine <koleervine@gmail.com>
2023-10-17 15:09:32 -0600

    file structure and folders

---
+++ MyWordle/MyWordle/CoreCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWordle
+//
+//  Created by kole ervine on 10/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
